748a8361f04ee13efe652aa937bb4144daa66afb
Filipa Correia <filipaincorreia@gmail.com>
2018-05-31 23:51:14 +0300

    Analysing hypernyms of white cards

--- feature_extract.py
+++ feature_extract.py
@@ -1,5 +1,8 @@
 import json
 import random
+import spacy
+import nltk
+from nltk import wordnet as wn
 from pprint import pprint
 
 
@@ -8,9 +11,29 @@ def load_whites():
         whites = json.load(f)['whiteCards']
 
     print("imported number of white cards: " + str(len(whites)))
-    print("example: " + random.choice(whites))
 
     return whites
 
 
 whites = load_whites()
+nlp = spacy.load('en_core_web_sm')
+dictionary = {}
+print 'hypernym example: ' + str(wn.wordnet.synset('clitoris.n.01').hypernyms())
+
+
+for sen in whites:
+	doc = nlp(sen)
+	for token in doc:
+		if token.pos_ == 'NOUN' and len(wn.wordnet.synsets(token.lemma_, pos='n')) > 0:
+			hypernyms = wn.wordnet.synsets(token.lemma_, pos='n')[0].hypernym_paths()[0]
+			max_index = len(hypernyms) - 1
+			if len(hypernyms) > 3:
+				max_index = 3
+			if hypernyms[max_index] in dictionary:
+				dictionary[hypernyms[max_index]] +=1
+			else:
+				dictionary[hypernyms[max_index]] =1
+			#print 'LEMMA: ' + str(token.lemma_) + ' HYPERNYM: ' + str(wn.wordnet.synset(token.lemma_ + '.n.01').hypernyms())
+
+
+pprint(dictionary)
